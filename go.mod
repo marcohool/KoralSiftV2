5caf0bc7a43718c2d7d1a88139516c2504d08c83
Marco <marco.boutchelski@gmail.com>
2025-02-13 01:32:56 +0000

    get html from all products page

--- go.mod
+++ go.mod
@@ -1,1 +1,15 @@
 module KoralSiftV2
+
+go 1.24.0
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20250210231439-aea867ea8506 // indirect
+	github.com/chromedp/chromedp v0.12.1 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
--- scraper/stores/zara.go
+++ scraper/stores/zara.go
@@ -1,9 +1,50 @@
 ﻿package stores
 
 import (
+	"context"
 	"fmt"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
+	"time"
 )
 
 func ScrapeZara() {
-	fmt.Println("Scraping Zara")
+	fmt.Println("Start Zara Scraper")
+
+	ScrapeProductsPage("https://www.zara.com/uk/en/man-all-products-l7465.html?v1=2443335")
+
+}
+
+func ScrapeProductsPage(url string) {
+	fmt.Printf("Scraping Zara products page %s", url)
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	var html string
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Sleep(2000*time.Millisecond),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			rootNode, err := dom.GetDocument().Do(ctx)
+
+			if err != nil {
+				return err
+			}
+
+			html, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
+
+			return err
+		}),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(html)
 }
