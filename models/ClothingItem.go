88ff9eb18871f83c3420cdb4f0225a3b03fafdbd
Marco <marco.boutchelski@gmail.com>
2025-02-13 00:29:33 +0000

    add clothing item model

---
+++ models/ClothingItem.go
@@ -0,0 +1,15 @@
+﻿package models
+
+type ClothingItem struct {
+	Name         string   `json:"name"`
+	Description  string   `json:"description"`
+	Brand        string   `json:"brand"`
+	Category     string   `json:"category"`
+	Colours      []string `json:"colours"`
+	Price        float64  `json:"price"`
+	CurrencyCode string   `json:"currencyCode"`
+	Gender       string   `json:"gender"`
+	ImageUrl     string   `json:"imageUrl"`
+	SourceUrl    string   `json:"sourceUrl"`
+	SourceRegion string   `json:"sourceRegion"`
+}
